27194d69d35201db6e6ad8d6bcb5f5bba3026ed3
Hiroto Igarashi <hirotoigarashi@gmail.com>
2023-06-27 02:08:47 +0900

    2023-6-27 update

--- options.lua
+++ options.lua
@@ -22,10 +22,10 @@
 -- If you need more control, you can use the function()...end notation
 return function(local_vim)
   local_vim.opt.relativenumber = true -- sets vim.opt.relativenumber
-  local_vim.opt.number = true         -- sets vim.opt.number
-  local_vim.opt.spell = false         -- sets vim.opt.spell
-  local_vim.opt.signcolumn = "auto"   -- sets vim.opt.signcolumn to auto
-  local_vim.opt.wrap = true           -- sets vim.opt.wrap
+  local_vim.opt.number = true -- sets vim.opt.number
+  local_vim.opt.spell = false -- sets vim.opt.spell
+  local_vim.opt.signcolumn = "auto" -- sets vim.opt.signcolumn to auto
+  local_vim.opt.wrap = true -- sets vim.opt.wrap
   local_vim.opt.showtabline = 2
   local_vim.opt.shiftwidth = 2
   local_vim.opt.tabstop = 2
@@ -34,16 +34,30 @@ return function(local_vim)
   local_vim.opt.smartindent = true
   local_vim.opt.tabstop = 2
   local_vim.opt.textwidth = 50
+  local_vim.opt.colorcolumn = "80"
   -- local_vim.opt.formatoptions += mM
-  local_vim.opt.whichwrap = vim.opt.whichwrap - { "b", "s" } -- removing option from list
-  local_vim.opt.shortmess = vim.opt.shortmess + { I = true } -- add to option list
-  local_vim.g.mapleader = " "                                -- sets vim.g.mapleader
-  local_vim.g.autoformat_enabled = true                      -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-  local_vim.g.autoformat_enabled = true                      -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-  local_vim.g.cmp_enabled = true                             -- enable completion at start
-  local_vim.g.autopairs_enabled = true                       -- enable autopairs at start
-  local_vim.g.diagnostics_mode = 3                           -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-  local_vim.g.icons_enabled = true                           -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+  -- removing option from list
+  local_vim.opt.whichwrap = vim.opt.whichwrap - { "b", "s" }
+  -- add to option list
+  local_vim.opt.shortmess = vim.opt.shortmess + { I = true }
+  -- sets vim.g.mapleader
+  local_vim.g.mapleader = " "
+  -- enable or disable auto formatting at start
+  -- (lsp.formatting.format_on_save must be enabled)
+  local_vim.g.autoformat_enabled = true
+  -- enable or disable auto formatting at start
+  -- (lsp.formatting.format_on_save must be enabled)
+  local_vim.g.autoformat_enabled = true
+  -- enable completion at start
+  local_vim.g.cmp_enabled = true
+  -- enable autopairs at start
+  local_vim.g.autopairs_enabled = true
+  -- set the visibility of diagnostics in the UI
+  -- (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+  local_vim.g.diagnostics_mode = 3
+  -- disable icons in the UI
+  -- (disable if no nerd font is available, requires :PackerSync after changing)
+  local_vim.g.icons_enabled = true
 
   return local_vim
 end
